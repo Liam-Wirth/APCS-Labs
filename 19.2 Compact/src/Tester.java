813c61f9a302e1b257b851ae84460549cc0966a6
Liam <liam.wirth1@outlook.com>
2022-01-19 11:17:10 -0800

    the start of gridworld

---
+++ 19.2 Compact/src/Tester.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Tester { //I do not want to do a comment header I wrote my own code this is me leave me alone.
+     //you say peeCock, and  no one bats an eye
+     //you say poopCock and everyone loses their mind
+    File  compact = new File(compact.txt); //importing the file and appending the name compact cause I want it to have that name.
+    public int[] ingest() throws FileNotFoundException {
+       Reader reader = new FileReader(compact);
+        int i= (int) compact.length(); //bad practice I know blah blah blah, in this case I know length is short
+
+        int[] peeCock = new int[i];
+        reader.read(peeCock);
+        for (int j = 0; j < peeCock.length; j++) {
+
+        }
+        return peeCock; //you say peeCock, and  no one bats an eye
+
+
+    }
+    public int[] removeZeros(int [] peeCock){
+        int[] poopCock = new int[p]
+
+        }
+    }
+
+}

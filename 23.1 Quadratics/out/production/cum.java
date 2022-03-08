edd886d3b783dbcc2ba077cd11b45dabe0bf415d
Liam-Wirth <liam.wirth1@outlook.com>
2022-03-07 20:28:30 -0800

    bashing head into keyboard

---
+++ 23.1 Quadratics/out/production/cum.java
@@ -0,0 +1,40 @@
+package out.production;
+import org.w3c.dom.ls.LSOutput;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class cum {
+    public cum() {
+        initComponents();
+    }
+
+    private void initComponents() {
+        // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
+        // Generated using JFormDesigner Evaluation license - unknown
+        var panel1 = new JPanel();
+        var label1 = new JLabel();
+
+        //======== panel1 ========
+        {
+            panel1.setName("VisualSortingAlgorithms!");
+            panel1.setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.
+            swing.border.EmptyBorder(0,0,0,0), "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn",javax.swing.border
+            .TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java.awt.Font("Dia\u006cog"
+            ,java.awt.Font.BOLD,12),java.awt.Color.red),panel1. getBorder
+            ()));panel1. addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java
+            .beans.PropertyChangeEvent e){if("\u0062ord\u0065r".equals(e.getPropertyName()))throw new RuntimeException
+            ();}});
+            panel1.setLayout(new GridBagLayout());
+
+            //---- label1 ----
+            label1.setHorizontalAlignment(SwingConstants.CENTER);
+            label1.setText("VisualSorting");
+            panel1.add(label1, new GridBagConstraints(1, 0, 1, 1, 0.0, 0.0,
+                GridBagConstraints.CENTER, GridBagConstraints.BOTH,
+                new Insets(1, 1, 1, 3), 0, 0));
+        }
+        // JFormDesigner - End of component initialization  //GEN-END:initComponents
+    }
+
+}
